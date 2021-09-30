84d15f064e729e5769461451f73d2463a44f791b
Daniel Kim <danielkim2711@gmail.com>
2021-10-01 11:33:15 +1300

    Define a simple unit test

--- app/src/test/java/cs/daniel/bookingapp/TimeslotTest.java
+++ app/src/test/java/cs/daniel/bookingapp/TimeslotTest.java
@@ -1,10 +1,13 @@
 package cs.daniel.bookingapp;
 
+import static org.junit.Assert.assertEquals;
+
 import junit.framework.TestCase;
 
 public class TimeslotTest extends TestCase {
 
     public void testBookTimeSlot() {
+        assertEquals(4, 2 + 2);
     }
 
 }
